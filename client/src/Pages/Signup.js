c72e3b2dc858800608be7bccebf7c4c001252c05
Harman Dhiman <harmandhiman6776@gmail.com>
2025-06-20 21:44:04 +0530

    signup frontend done , time to start its backend

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -3,15 +3,15 @@ import "../Styles/Signup.css";
 
 const Signup = () => {
     return (
-        <div className="Signup">
+        <div className="signup-page"> {/* Parent class for scoping */}
             <div className="signup-container">
-                <h1 className='signup-heading'> Sign Up</h1>
+                <h1 className='signup-heading'>Sign Up</h1>
                 <div className="signup-form">
-                     <input type="text"  placeholder='Name' required/>
-                      <input type="text"  placeholder='Email required'/>
-                    <input type="text"  placeholder='Password' required/>
-                    <input type="text"  placeholder='Confirm Password' required/>
-                    <button className='signup-button' type='submit'>Signup</button>
+                    <input type="text" placeholder='Name' required />
+                    <input type="email" placeholder='Email' required />
+                    <input type="password" placeholder='Password' required />
+                    <input type="password" placeholder='Confirm Password' required />
+                    <button className='signup-button' type='submit'>Sign Up</button>
                 </div>
             </div>
         </div>
